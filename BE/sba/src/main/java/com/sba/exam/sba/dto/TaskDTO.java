7e0424773aab77dff21e925ad5258ba7a7548f6e
Gatsby204 <Hieuptse172845@fpt.edu.vn>
2025-02-26 10:44:20 +0700

    Format Date Task

--- BE/sba/src/main/java/com/sba/exam/sba/dto/TaskDTO.java
+++ BE/sba/src/main/java/com/sba/exam/sba/dto/TaskDTO.java
@@ -1,5 +1,6 @@
 package com.sba.exam.sba.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,13 @@ import java.util.Date;
 @AllArgsConstructor
 public class TaskDTO {
     private int taskId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date completedAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date dueDate;
     private String taskDescription;
     private int taskStatusId;
